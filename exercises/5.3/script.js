d0f39b80b707822e1f90b341701771ca6769eb4e
edison-ps <edison@osite.com.br>
2020-12-06 10:43:29 -0300

    Exercicio 003

--- exercises/5.3/script.js
+++ exercises/5.3/script.js
@@ -24,29 +24,47 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-
   let docDays = document.querySelector('#days');
 
-  for (indexDays in dezDaysList) {
+  for (let indexDays = 0; indexDays < dezDaysList.length; indexDays += 1) {
 
     let addDays = document.createElement('li');
     addDays.innerText = dezDaysList[indexDays];
     addDays.className = 'day';
 
     if (dezDaysList[indexDays] === 24 || dezDaysList[indexDays] === 25 || dezDaysList[indexDays] ===31) {
-
       addDays.className += ' holiday';
-
     }
 
     if (dezDaysList[indexDays] === 4 || dezDaysList[indexDays] === 11 || dezDaysList[indexDays] === 18 || dezDaysList[indexDays] === 25) {
-
       addDays.className += ' friday';
 
     }
-
     docDays.appendChild(addDays);
-
   }
 
   makeHoliday('Feriados');
+  const btnHolyday = document.querySelector('#btn-holiday');
+  let displayHoliday = false;
+
+  btnHolyday.addEventListener('click', function () {
+      const holidayBackground = document.querySelectorAll('.holiday');
+
+      for (let index = 0; index < holidayBackground.length; index += 1) {
+
+        if (displayHoliday) {
+          holidayBackground[index].style.backgroundColor = 'rgb(238, 238, 238)';
+        }
+        else {
+          holidayBackground[index].style.backgroundColor = 'rgb(255, 255, 255)';
+        }
+    }
+
+    if (displayHoliday) {
+      displayHoliday  = false;
+    }
+    else {
+      displayHoliday  = true;
+    }
+
+  });
