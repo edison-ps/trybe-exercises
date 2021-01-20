d5d031f417198208c181fc3a2301a4defec4e0be
edison-ps <edison@osite.com.br>
2021-01-20 19:01:57 -0300

    exercicio005 V1.00

---
+++ exercises/8.3/exercicio005.js
@@ -0,0 +1,19 @@
+
+const assert = require('assert');
+
+const names = [
+  'Aanemarie',  'Adervandes',   'Akifusa',
+  'Abegildo',   'Adicellia',    'Aladonata',
+  'Abeladerco', 'Adieidy',  'Alarucha',
+];
+
+
+
+function containsA() {
+  // escreva seu código aqui
+  return names.reduce((accumulator, currentValue) => {
+    return currentValue.split('a').length + accumulator;
+  }, 0);
+}
+
+assert.deepStrictEqual(containsA(), 20);
